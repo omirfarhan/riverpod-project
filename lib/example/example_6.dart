9cf202cc8e740ea73c3970f7c76a7297283faf6f
omirfarhan <omirfarhan1@gmail.com>
2026-05-06 20:56:26 +0600

    example 6 is complete

--- lib/example/example_6.dart
+++ lib/example/example_6.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:hooks_riverpod/legacy.dart';
+import 'package:hooks_riverpod/misc.dart';
+import 'package:riverpod/riverpod.dart';
 
 enum FavoriteStatus { All, Favorite, NotFavorite }
 
@@ -12,12 +14,73 @@ final allFilmsProvider = StateNotifierProvider<FilmNotifier, List<Film>>(
   (_) => FilmNotifier(),
 );
 
+final favoriteFilmProvider = Provider<Iterable<Film>>((ref) {
+  final film = ref.watch(allFilmsProvider);
+  return film.where((film) => film.isFavourite);
+});
+
+final NotfavoriteFilmProvider = Provider<Iterable<Film>>((ref) {
+  final film = ref.watch(allFilmsProvider);
+  return film.where((film) => !film.isFavourite);
+});
+
 class Example6 extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(title: const Text('Example 6')),
-      body: Column(children: [const FilterWidget()]),
+      body: Column(
+        children: [
+          const FilterWidget(),
+          Consumer(
+            builder: (context, ref, child) {
+              final filter = ref.watch(favoriteStatusProvider);
+              switch (filter) {
+                case FavoriteStatus.All:
+                  return FilmList(provider: allFilmsProvider);
+
+                case FavoriteStatus.Favorite:
+                  return FilmList(provider: favoriteFilmProvider);
+                case FavoriteStatus.NotFavorite:
+                  return FilmList(provider: NotfavoriteFilmProvider);
+              }
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class FilmList extends ConsumerWidget {
+  final ProviderListenable<Iterable<Film>> provider;
+  const FilmList({required this.provider, super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final film = ref.watch(provider);
+    return Expanded(
+      child: ListView.builder(
+        itemCount: film.length,
+        itemBuilder: (context, index) {
+          final films = film.elementAt(index);
+          final favoriteIcon = films.isFavourite
+              ? const Icon(Icons.favorite)
+              : const Icon(Icons.favorite_border);
+
+          return ListTile(
+            title: Text(films.title),
+            subtitle: Text(films.description),
+            trailing: IconButton(
+              onPressed: () {
+                final isFavorite = !films.isFavourite;
+                ref.read(allFilmsProvider.notifier).update(films, isFavorite);
+              },
+              icon: favoriteIcon,
+            ),
+          );
+        },
+      ),
     );
   }
 }
@@ -64,7 +127,7 @@ class Film {
     id: id,
     title: title,
     description: description,
-    isFavourite: isFavourite,
+    isFavourite: isFavorite,
   );
 
   @override
